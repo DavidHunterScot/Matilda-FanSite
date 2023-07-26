46c4bf25d95e72500f76b67181d09fe1a3478b1c
DavidHunterScot <david.hunter@echoverse.cc>
2023-07-26 17:05:02 +0100

    Added Coming Soon page

---
+++ src/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Matilda - Fan Site</title>
+    </head>
+    <body style="background-color: #f2f2f2;">
+        <div style="text-align: center; font-family: sans-serif;">
+            <h1>Matilda</h1>
+            <p><i>A fan-made website for a fictional character, the precocious child known as Matilda Wormwood.</i></p>
+            <hr>
+            <h2>Coming Soon</h2>
+            <p>Watch this space.</p>
+        </div>
+    </body>
+</html>
